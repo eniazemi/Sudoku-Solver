d2c6473b007ed32166e9013cddc9aeb572d6c5d4
Eni01 <eni.azemi@gmail.com>
2023-05-10 14:27:41 +0200

    Code now run like a function. Still need to make some improvements

--- readTable.py
+++ readTable.py
@@ -1,5 +1,19 @@
 import pandas as pd
 
-df = pd.read_excel('testExcel.xlsx', sheet_name='Sheet1')
-df = df.fillna(0)
-matrix = df.to_numpy()
+# to be added :
+# file not found error : if path doesn't exist
+# check the total number of numbers given.
+# check if the numbers are given in correct form. no str char bool
+# check if all the numbers are whole int
+# read from csv, txt
+
+
+def read_table(name_of_table):
+    df = pd.read_excel(name_of_table + '.xlsx', sheet_name='Sheet1')
+    df = df.fillna(0)
+    df = df.astype(int)
+    matrix = df.to_numpy()
+    return matrix
+
+
+x = read_table("testExcel")
