2d75157940425632eb0249e0a63b00b9ec6c90ca
Dreni Mujaj <dreni.mujaj@elba-tech.com>
2023-05-14 18:57:44 +0200

    debugging and adding a part of the code to test the current implementation of iteration dfs

--- functions.py
+++ functions.py
@@ -60,35 +60,84 @@ def read_table(path):
 
 
 
-def solve_sudoku(board):
-    stack = [(board, 0, 0)]
-    while stack:
-        curr_board, row, col = stack.pop()
-        if row == 9:
-            return curr_board
-        if curr_board[row][col] != 0:
-            if col == 8:
-                stack.append((curr_board, row+1, 0))
-            else:
-                stack.append((curr_board, row, col+1))
-            continue
-        for num in range(1, 10):
-            if is_valid(curr_board, row, col, num):
-                curr_board[row][col] = num
-                if col == 8:
-                    stack.append((curr_board, row+1, 0))
-                else:
-                    stack.append((curr_board, row, col+1))
-        curr_board[row][col] = 0
-    return None
-
-
 def is_valid(board, row, col, num):
+    # Check if 'num' is already present in the current row
     for i in range(9):
         if board[row][i] == num:
             return False
+
+    # Check if 'num' is already present in the current column
+    for i in range(9):
         if board[i][col] == num:
             return False
-        if board[3*(row//3) + i//3][3*(col//3) + i%3] == num:
-            return False
+
+    # Check if 'num' is already present in the current 3x3 grid
+    start_row = 3 * (row // 3)
+    start_col = 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+
     return True
+
+
+def find_unassigned_location(board):
+    # Find an unassigned location on the board
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                return row, col
+    return -1, -1  # If no unassigned location is found
+
+
+def solve_sudoku(board):
+    # Find an unassigned location
+    row, col = find_unassigned_location(board)
+
+    # If no unassigned location is found, the puzzle is solved
+    if row == -1 and col == -1:
+        return True
+
+    # Try different numbers in the unassigned location
+    for num in range(1, 10):
+        if is_valid(board, row, col, num):
+            board[row][col] = num
+
+            # Recursively solve the puzzle
+            if solve_sudoku(board):
+                return True
+
+            # If the current configuration doesn't lead to a solution,
+            # undo the current assignment and try a different number
+            board[row][col] = 0
+
+    return False
+
+
+def print_board(board):
+    # Print the Sudoku board
+    for i in range(9):
+        for j in range(9):
+            print(board[i][j], end=" ")
+        print()
+
+
+# Sample Sudoku board (0 represents empty cells)
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if solve_sudoku(board):
+    print("Sudoku solved:")
+    print_board(board)
+else:
+    print("No solution exists for the")
